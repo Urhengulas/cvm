18468421a8ec6b7d2fd7f67bfd99d8860de36dcd
Johann Hemmann <johann.hemmann@ferrous-systems.com>
2024-05-21 15:09:01 +0200

    Correct implementation (but it looks ugly)

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,58 @@
-use std::fs;
+//! CVM distinct element estimation algorithm
+//!
+//! - `A` - stream of elements {a1, a2, ..., am} or set of elements in the stream
+//! - `|A|` - the size of that set, with multiplicities ignored
+//! - `At` - the first t elements of that stream, ignoring multiplicity
+//! - `Bt` - buffer B that remembers a randomly chosen subset of thise elements at time t
+//! - `pt` - |Bt|/pt is an unbiased estimate of |At|
+//! - `s` - maximum buffer size
+
+use std::{collections::HashSet, fs};
 
 fn main() {
-    let words = read_words();
+    let input = read_words();
+    let estimate = estimate_distinct_elements(&input);
+    let correct = input.into_iter().collect::<HashSet<_>>().len();
+    dbg!(estimate, correct);
+}
 
-    let mut unique_words = CountList::new();
-    let mut round_counter = 0;
-    const N: usize = 1000;
-
-    for word in words {
-        match unique_words.insert(word) {
-            // the word is known already
-            Some(idx) => {
-                for _ in 0..round_counter {
-                    // flip the coin to see if we should keep the word
-                    if flip_coin() {
-                        unique_words.remove(idx);
-                        break;
-                    }
-                }
-            }
-            None => {} // it's a new word
-        }
-
-        // drop random words when list is full
-        if unique_words.len() >= N {
-            unique_words.end_round();
-            round_counter += 1;
-        }
+fn estimate_distinct_elements(a_set: &[String]) -> u32 {
+    const PRECISION: u32 = 100_000;
+    fn flip_coin() -> u32 {
+        rand::random::<u32>() % PRECISION
     }
 
-    dbg!(unique_words.len(), round_counter);
+    // D1
+    let mut p = PRECISION;
+    let mut b_set: Vec<(String, u32)> = Vec::new();
+    let s = 500;
+
+    // D2 & D3
+    for a_t in a_set {
+        // D4
+        b_set.retain(|(a, _)| a != a_t);
+
+        // D5
+        let u_t = flip_coin();
+        if u_t >= p {
+            continue;
+        }
+        if b_set.len() < s {
+            b_set.push((a_t.clone(), u_t));
+            continue;
+        }
+
+        // D6
+        let (a_max, u_max) = b_set.iter_mut().max_by_key(|b| b.1).unwrap();
+        if u_t > *u_max {
+            p = u_t;
+        } else {
+            p = *u_max;
+            *a_max = a_t.clone();
+            *u_max = u_t;
+        }
+    }
+    (b_set.len() as u32) * PRECISION / p
 }
 
 fn read_words() -> Vec<String> {
@@ -38,38 +61,4 @@ fn read_words() -> Vec<String> {
     let text = text.replace("--", " ");
     let text = text.to_lowercase();
     text.split_whitespace().map(Into::into).collect()
-}
-
-#[derive(Debug, Default)]
-struct CountList(Vec<String>);
-
-impl CountList {
-    fn new() -> Self {
-        Default::default()
-    }
-
-    fn insert(&mut self, value: String) -> Option<usize> {
-        if let Some(idx) = self.0.iter().position(|x| x == &value) {
-            Some(idx)
-        } else {
-            self.0.push(value);
-            None
-        }
-    }
-
-    fn len(&self) -> usize {
-        self.0.len()
-    }
-
-    fn end_round(&mut self) {
-        self.0.retain(|_| flip_coin());
-    }
-
-    fn remove(&mut self, index: usize) {
-        self.0.swap_remove(index);
-    }
-}
-
-fn flip_coin() -> bool {
-    rand::random()
 }
