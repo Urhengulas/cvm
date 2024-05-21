029fb6f12978e56cda0cbce1f2e1ae5bb6ae95fe
Johann Hemmann <johann.hemmann@ferrous-systems.com>
2024-05-21 11:58:28 +0200

    Count with HashSet (4844)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{collections::HashSet, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let words = read_words();
+    let unique_words = words.into_iter().collect::<HashSet<_>>();
+    dbg!(unique_words.len());
+}
+
+fn read_words() -> Vec<String> {
+    let text = fs::read_to_string("hamlet.txt").unwrap();
+    let text = text.replace(['.', ',', '?', ':', ';', '!'], " ");
+    let text = text.replace("--", " ");
+    let text = text.to_lowercase();
+    text.split_whitespace().map(Into::into).collect()
 }
